38e55b385002ab8b834d3408225c1abfa030d50e
Chris Cooke <chrooke@geekdo.org>
2016-03-05 20:30:42 -0500

    Continuing to upgrade to SDK v1.2.2.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import jibo from 'jibo';
-import path from 'path';
-import GuiManager from './mim/debug-gui-manager';
-import MimManager from './mim/mim-manager';
+"use strict";
 
-let {Status, factory} = jibo.bt;
+let jibo = require ('jibo');
+let path = require ('path');
+let GuiManager = require ('./mim/debug-gui-manager');
+let MimManager = require ('./mim/mim-manager');
+
+let Status, factory = jibo.bt.Status;
 
 let root = null;
 
@@ -11,11 +13,13 @@ let blackboard = {};
 let notepad = {};
 
 function start() {
-    root = factory.create('../behaviors/main', {
+    root = jibo.bt.create('../behaviors/main', {
         blackboard: blackboard,
         notepad: notepad
     });
+        console.log('done create root');
     root.start();
+        console.log('start root');
     window.requestAnimationFrame(update);
 }
 
@@ -28,29 +32,20 @@ function update() {
     }
 }
 
-function setup() {
-    console.log('Setup');
 
-    require('./behaviors/debug-behavior');
-    require('./behaviors/mim');
-    require('./behaviors/mim-gui');
-    let eyeElement = document.getElementById('eye');
-    jibo.visualize.createRobotRenderer(eyeElement, jibo.visualize.DisplayType.EYE);
+jibo.init(function() {
+  console.log('Setup');
 
-    let ui_div = document.getElementById('ui');
-    GuiManager.init(ui_div);
-    MimManager.init();
-    MimManager.setGuiManager(GuiManager);
+  require('./behaviors/debug-behavior');
+  require('./behaviors/mim');
+  require('./behaviors/mim-gui');
+  let eyeElement = document.getElementById('eye');
+  jibo.visualize.createRobotRenderer(eyeElement, jibo.visualize.DisplayType.EYE);
 
-    start();
-}
+  let ui_div = document.getElementById('ui');
+  GuiManager.init(ui_div);
+  MimManager.init();
+  MimManager.setGuiManager(GuiManager);
 
-jibo.init(setup);
-
-//jibo.init().then(() => {
-//
-//    setup();
-//
-//}).catch(e => {
-//    console.error(e);
-//});
+  start();
+});
